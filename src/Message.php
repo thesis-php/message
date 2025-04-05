3859eb3094cd814989b4fd592eb73b2d237d7bdb
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2025-04-05 15:22:22 +0300

    Use mixed as default type of Message:TResult

--- src/Message.php
+++ src/Message.php
@@ -8,6 +8,6 @@ namespace Thesis\Message;
  * A marker interface for messages. For queries, `TResult` specifies the expected handler result type.
  *
  * @api
- * @template-covariant TResult = null
+ * @template-covariant TResult = mixed
  */
 interface Message {}
